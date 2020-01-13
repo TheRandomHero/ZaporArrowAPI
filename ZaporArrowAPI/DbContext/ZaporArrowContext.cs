44516348ad51506997498f463c293112048eaff7
Attila Molnar <scythianhawk@gmai.com>
2020-01-13 13:24:09 +0100

    Entities
    Entities configured

---
+++ ZaporArrowAPI/DbContext/ZaporArrowContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ZaporArrowAPI.Entities;
+
+namespace ZaporArrow.DbContexts
+{
+    public class ZaporArrowContext : DbContext
+    {
+        public ZaporArrowContext(DbContextOptions<ZaporArrowContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Arrow> Arrows { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+    }
+}
